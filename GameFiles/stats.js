ded04e22627eac905b367b5d872ee519fd5099ec
Gforce <gforcedrop@gmail.com>
2020-02-07 17:50:25 -0500

    Html unlock complete

--- GameFiles/stats.js
+++ GameFiles/stats.js
@@ -11,6 +11,8 @@ var playerData = {
   playerCodingSkill: 1,
   playerLanguageSkillC: 0,
   playerLanguageSkillHTML: 0 ,
+  playerLanguageSkillHTMLExp: 0 ,
+  playerLanguageSkillHTMLExpNextLevel: 13 ,
   playerLanguageSkillCSS: 0 ,
   playerLanguageSkillJS: 0,
   playerLanguageSkillSQL: 0,
@@ -18,14 +20,27 @@ var playerData = {
   playerCloudSkill: 0
 }
 
+var firstUnlockLoop = window.setInterval(function(){
+  checkUpgrade()
+  if (playerData.playerKnowledgeSkill >= 5) {
+    window.clearInterval(firstUnlockLoop)
+  }
+},1000)
+
 //JS functions
 
 function buildStudySkill() {
   playerData.playerStudySkillExp += playerData.playerStudySkill
   playerData.playerKnowledgeSkill += playerData.playerStudySkill/10
-  updateDisplay()
   levelUpStudy()
-  checkUpgrade()
+  updateDisplay()
+}
+
+function buildHTMLSkill() {
+  playerData.playerLanguageSkillHTMLExp += playerData.playerStudySkill
+  playerData.playerKnowledgeSkill += playerData.playerStudySkill/10
+  levelUpHTML()
+  updateDisplay()
 }
 
 function levelUpStudy(){
@@ -36,16 +51,35 @@ function levelUpStudy(){
   }
 }
 
+function levelUpHTML(){
+  if(playerData.playerLanguageSkillHTMLExp >= playerData.playerLanguageSkillHTMLExpNextLevel ){
+    playerData.playerLanguageSkillHTML++
+    playerData.playerLanguageSkillHTMLExp -= playerData.playerLanguageSkillHTMLExpNextLevel
+    playerData.playerLanguageSkillHTMLExpNextLevel = Math.round(playerData.playerLanguageSkillHTMLExpNextLevel*=1.5)
+  }
+}
+
 //Update main.html Display
 function updateDisplay(){
-  document.getElementById('knowledgeDisplay').innerHTML = playerData.playerKnowledgeSkill.toFixed(2)
-  document.getElementById('studyDisplay').innerHTML = playerData.playerStudySkill
-  document.getElementById('studyExpDisplay').innerHTML = playerData.playerStudySkillExp
-  document.getElementById('studyExpNLDisplay').innerHTML = playerData.playerStudySkillNextLevel
+  document.getElementById('knowledgeDisplay').innerHTML = "Knowledge " + playerData.playerKnowledgeSkill.toFixed(2)
+  document.getElementById('studyDisplay').innerHTML = "Study Level " + playerData.playerStudySkill
+  document.getElementById('studyExpDisplay').innerHTML = "Study EXP " + playerData.playerStudySkillExp
+  document.getElementById('studyExpNLDisplay').innerHTML = "Study Exp for Next Level " + playerData.playerStudySkillNextLevel
+  document.getElementById('htmlDisplay').innerHTML = "HTML Level " + playerData.playerLanguageSkillHTML
+  document.getElementById('htmlExpDisplay').innerHTML = "HTML EXP " + playerData.playerLanguageSkillHTMLExp
+  document.getElementById('htmlExpNLDisplay').innerHTML = "HTML Exp for Next Level " + playerData.playerLanguageSkillHTMLExpNextLevel
 }
 //Update unlocks
 function checkUpgrade() {
   if(playerData.playerKnowledgeSkill >= 5){
     document.getElementById('upgrade').style.display = "block"
   }
+}
+
+function learnHTML(){
+  document.getElementById('upgrade').style.display = "none"
+  document.getElementById('htmlDisplay').style.display = "block"
+  document.getElementById('htmlExpDisplay').style.display = "block"
+  document.getElementById('htmlExpNLDisplay').style.display = "block"
+    document.getElementById('learnHTMLBTN').style.display = "block"
 }
